f8233cdef6402f4e47dca0f88f28e56047c953b5
ella.exid <ella.exid@kakaoenterprise.com>
2020-03-03 09:34:16 +0900

    feat: add NotFoundComponent by codepen

--- material-project/src/app/app.module.ts
+++ material-project/src/app/app.module.ts
@@ -1,31 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { HelloComponent } from './hello/hello.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HelloComponent } from './hello/hello.component';
 import { TimedisplayComponent } from './timedisplay/timedisplay.component';
 import { TimeDisplayModule } from './timedisplay/timedisplay.module';
+import { NotFoundComponent } from './notfound/notfound.component';
 
 const appRoutes: Routes = [
-  // { path: '**', component: NotFoundComponent },
   { path: '', component: HomeComponent },
   { path: 'hello', component: HelloComponent, data: { name: 'Hello' } },
   { path: 'timer', component: TimedisplayComponent, data: { name: 'Timer' } },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HelloComponent
+    HelloComponent,
+    NotFoundComponent
   ],
   imports: [
     RouterModule.forRoot(
