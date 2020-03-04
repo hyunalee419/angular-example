ced596454fb6728ecee387ee9a584e9327420847
ella.exid <ella.exid@kakaoenterprise.com>
2020-03-04 17:33:14 +0900

    refactor: mv fonts

--- material-project/src/app/hello/hello.component.ts
+++ material-project/src/app/hello/hello.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
 
